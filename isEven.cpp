ab2345820137d6546bbf3de66500ff5d9a0b42b2
Julian-Farkas <julian.farkas.ger@gmail.com>
2022-11-14 00:02:27 +0100

    added functionality

--- isEven.cpp
+++ isEven.cpp
@@ -3,8 +3,12 @@
 ** If not, it returns 0
 */
 
-int main(int argc, char const *argv[])
+bool main(int argc, char const *argv[])
 {
-    
-    return 0;
+    /* If last bit of arg is 1 (arg & 1), the number is not even. 
+    ** By negating the expression the return value is 0 instead of one,
+    ** if the number is not even
+    */
+
+    return !(*argv[1] & 1);
 }
